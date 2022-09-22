bbac47ca6bc17194759decf6343ca302992d500e
Charles VIGNAL <charles.vignal@gmail.com>
2022-09-22 12:16:20 +0200

    [mod] small modif to polybar look

--- local/bin/polybar/net_speed.sh
+++ local/bin/polybar/net_speed.sh
@@ -8,8 +8,8 @@
 source colors.sh
 
 net_speed() {
-	UP_NETSPEED=$(awk '{if(l1){printf "%6.1f", ($10-l2)/1024} else{l1=$2; l2=$10;}}' <(grep wlan0 /proc/net/dev) <(sleep 1; grep wlan0 /proc/net/dev))
-	DOWN_NETSPEED=$(awk '{if(l1){printf "%6.1f", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlan0 /proc/net/dev) <(sleep 1; grep wlan0 /proc/net/dev))
+	UP_NETSPEED=$(awk '{if(l1){printf "%6.1f", ($10-l2)/1024} else{l1=$2; l2=$10;}}' <(grep wlp3s0 /proc/net/dev) <(sleep 1; grep wlp3s0 /proc/net/dev))
+	DOWN_NETSPEED=$(awk '{if(l1){printf "%6.1f", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlp3s0 /proc/net/dev) <(sleep 1; grep wlp3s0 /proc/net/dev))
 	# echo "$dark0_soft [ $faded_aqua $UP_NETSPEED KB/s$RESET  $faded_green↓ $DOWN_NETSPEED KB/s$RESET $dark0_soft]"
 	echo "$dark0_soft $faded_aqua  $UP_NETSPEED KB/s$RESET    $faded_green   $DOWN_NETSPEED KB/s$RESET $dark0_soft"
 }
